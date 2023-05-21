a89adc41eb9171f9a3cd5906e15f314c9c9c0496
sicArchitect <maciej.kusztal1995@gmail.com>
2023-05-21 11:37:16 +0300

    Add routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import bookRoutes from "./routes/books.routes";
 
 const app = express();
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello M");
+app.use("/", bookRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Books API");
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "404 not found..." });
 });
 
 const server = app.listen(3000, () => {
